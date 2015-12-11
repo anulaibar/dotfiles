602071a0a4d4eb98773e500e136698b5d646eb02
Olle Hellgren <olle.hellgren@blocket.se>
2015-12-11 16:10:04 +0100

    Moved git proxy before vundle install

--- setup.sh
+++ setup.sh
@@ -5,14 +5,12 @@ echo "source .profile" >> ~/.bash_profile
 
 cp ~/utils/.vimrc ~/.vimrc
 
+git config --global http.proxy devproxy:3128
+git config --global https.proxy devproxy:3128
+npm config set proxy devproxy:3128
+npm config set https-proxy devproxy:3128
+
 if [[ ! -d ~/.vim/bundle ]]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
-
-npm config set proxy devproxy:3128
-npm config set https-proxy devproxy:3128
-
-git config --global http.proxy devproxy:3128
-git config --global https.proxy devproxy:3128
-
